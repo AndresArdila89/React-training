18535089965701b2880261c7486bfe74ab8d7d3c
AndresArdila89 <andresardila@me.com>
2020-10-21 23:20:07 -0400

    first commit

---
+++ module_3/project-one/src/Person/Person.js
@@ -0,0 +1,9 @@
+//ES6
+
+import React from 'react';
+
+const person = (props) =>{
+    return <p> I'm a Person!! and I am {props.name} and I'm {props.age} years old </p>
+};
+
+export default person;
